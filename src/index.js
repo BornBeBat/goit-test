e3b93059e83f0fe462a7aa9b30175df77f2966ef
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-02-23 14:45:17 +0200

    strict mode on

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter basename="/goit-test">
-      <App />
-    </BrowserRouter>
-  </Provider>
-  //  </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename="/goit-test">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
