87c207ddff835fc86adccbc86ba0885693c6cffa
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-02-23 15:31:01 +0200

    regExp for 1million+

--- src/utils/trallingComa.js
+++ src/utils/trallingComa.js
@@ -1,5 +1,5 @@
 export const trallingComa = number => {
   const str = number + '';
-  const reg = new RegExp(/(?<=\d)(\d{3}$)/gm);
-  return str.replace(reg, ',$1');
+  const reg = new RegExp(/(?<=\d)(\d{3})(?=\d{3}$)|(?<=\d)(\d{3}$)/gm);
+  return str.replace(reg, `,$&`);
 };
